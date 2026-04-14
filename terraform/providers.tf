8ae02d0b824fef072acc6264e484a4ed75e71128
franferrazdevsec <fran.ferraz.dev.sec@gmail.com>
2026-04-14 19:49:10 -0300

    Create providers.tf
    Explicação:
    ●        O Terraform usará os providers oficiais kubernetes e helm, conectando-se ao cluster Kind por meio do kubeconfig local (~/.kube/config), que o Kind já gera ao criar o cluster. 
    ●        Isso permite aplicar manifests, instalar charts e provisionar serviços diretamente pelo Terraform — com rastreabilidade.

---
+++ terraform/providers.tf
@@ -0,0 +1,19 @@
+terraform { 
+required_providers { 
+kubernetes = { 
+source  = "hashicorp/kubernetes" 
+version = "~> 2.19" 
+} helm = { 
+source  = "hashicorp/helm" 
+version = "~> 2.12" 
+  } 
+ } 
+} 
+provider "kubernetes" { 
+config_path = "~/.kube/config" 
+}
+provider "helm" { 
+kubernetes { 
+config_path = "~/.kube/config" 
+ } 
+}
